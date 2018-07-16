d16db57d6ee5370379f8f5b30e1968cbc799632a
Hannes Hesse <hhesse@newstore.com>
2018-07-16 11:24:14 +0200

    Loading and display questions in list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "./App.css";
 
 const Polls = () => (
   <Router>
@@ -19,7 +18,49 @@ const Polls = () => (
   </Router>
 );
 
-const Questions = () => <h1>Questions</h1>;
+class Questions extends React.Component {
+  state = {
+    loading: false
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch("https://polls.apiblueprint.org/questions")
+      .then(response => {
+        return response.json();
+      })
+      .then(questions => {
+        this.setState({ questions, loading: false });
+      })
+      .catch(error => {
+        console.error(error);
+        this.setState({ error: true });
+      });
+  }
+
+  render() {
+    const { questions } = this.state;
+    return questions ? (
+      questions.map((question, index) => (
+        <QuestionLink question={question} key={index} />
+      ))
+    ) : (
+      <div>Loading...</div>
+    );
+  }
+}
+
+const QuestionLink = ({
+  question: { question, published_at, choices, url }
+}) => (
+  <div>
+    <a href={url}>
+      <h3>{question}</h3>
+    </a>
+    <h4>{published_at}</h4>
+    <h4>{choices.length} choices</h4>
+  </div>
+);
 
 const Question = ({ match }) => (
   <div>
